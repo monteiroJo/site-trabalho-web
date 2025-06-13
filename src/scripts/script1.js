38e7052d94a20b6e064f1c2c548fef3d7509de0f
jjos <164789644+monteiroJo@users.noreply.github.com>
2025-06-13 04:37:16 -0300

    p

--- src/scripts/script1.js
+++ src/scripts/script1.js
@@ -27,7 +27,26 @@ function hideUnhideLogin() {
     
     hideUnhide("login-block__");
     document.getElementById("header-section__").style.filter = k;
-    document.getElementById( "main-section__" ).style.filter = k;
     document.getElementById("footer-section__").style.filter = k;
+    document.getElementById( "main-section__" ).style.filter = k;
 }
-document.getElementById("main-section__").style.marginTop='50px';
+function hideUnhideLoginB() {
+    let k = document.getElementById("header-section__").style.filter;
+    k = k === "blur(5px)" ? "none" : "blur(5px)";
+    
+    hideUnhide("login-block__");
+    document.getElementById("header-section__").style.filter = k;
+    document.getElementById("footer-section__").style.filter = k;
+    document.getElementById( "noticia-main" ).style.filter = k;
+}
+function hhh() {
+    let kkkkk = document.getElementById('grid-header');
+    if (kkkkk.classList.contains('yes')) {
+        kkkkk.classList.remove('yes');
+        return;
+    } 
+    kkkkk.classList.add('yes');
+    // document.getElementById('grid-header').classList.remove('yes');
+}
+// document.getElementById("main-section__").style.marginTop='50px';
+// document.querySelector(".grid-area-c").style.display="none"
