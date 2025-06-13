639f776a176cf66b7bb3838be9e4c23f19d26617
jjos <164789644+monteiroJo@users.noreply.github.com>
2025-06-13 06:13:34 -0300

    b

--- src/scripts/script1.js
+++ src/scripts/script1.js
@@ -50,3 +50,61 @@ function hhh() {
 }
 // document.getElementById("main-section__").style.marginTop='50px';
 // document.querySelector(".grid-area-c").style.display="none"
+let logou = false;
+function submitSignUp() {
+    document.getElementById('register-form').addEventListener(
+        "submit", function(event){
+            event.preventDefault();
+            let username = document.getElementById("nome").value;
+            let email = document.getElementById("email").value;
+            let password = document.getElementById("password").value;
+            let passwdconf = document.getElementById("password-confirm").value;
+
+            if (password != passwdconf) {
+                alert("senhas não são as mesmas"); return;
+            }
+            const user = {
+                username: username,
+                email: email,
+                password: password,
+                passwdconf: passwdconf,
+            };
+            localStorage.setItem(email, JSON.stringify(user));
+            alert("feito com sucesso");
+            window.location.href = "home.html";
+        }
+    )
+}
+function logInSubmit() {
+    // log-in-form email password
+    document.getElementById("log-in-form")
+    .addEventListener("submit", function(event) {
+        event.preventDefault();
+        let email = document.getElementById("email").value;
+        let password = document.getElementById("password").value;
+
+        let user = localStorage.getItem(email);
+
+        if (user) {
+            let parsedUser = JSON.parse(user);
+            if (parsedUser.password === password) {
+                localStorage.setItem("user", JSON.stringify(parsedUser));
+                window.location.href = "home.html";
+            } else {
+                alert("senha incorreta");
+            }
+        } else {
+            alert("esse usuário não foi encontrado");
+        }
+    })
+}
+
+if (document.getElementById("header-log-in") != null && localStorage.getItem("user") ) {
+    document.getElementById("header-log-in").style.display="none"
+}
+let thispage = location.pathname.split("/").slice(-1)[0];
+if (thispage === "usuario.html") {
+    let hah = JSON.parse(localStorage.getItem("user"));
+    document.getElementById("nome").innerHTML = hah.username;
+    document.getElementById("email").innerHTML = hah.email;
+}
